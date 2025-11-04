66e4c8a8017778e7cfd8c9e1babfe84e73cea22b
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2025-11-04 16:01:51 +0200

    theme setup for echovalut app

---
+++ src/infastructure/theme/colors.js
@@ -0,0 +1,23 @@
+export const colors = {
+	text: {
+		primary: "#FAFAFA",
+		secondary: "#161819",
+		neutral: "#746161",
+		active: "#4FD3FD",
+		disabled: "#9C9C9C",
+		inverse: "#FFFFFF",
+		error: "#D0421B",
+		success: "#138000",
+	},
+	icons: {
+		default: "#FAFAFA",
+		active: "#4FD3FD",
+		disabled: "#9C9C9C",
+		error: "#D0421B",
+	},
+	bg: {
+		primary: "hsla(250, 80%, 66%, 0.8)",
+		secondary: "#FAFAFA",
+		neutral: "#D9D9D9",
+	},
+};
---
+++ src/infastructure/theme/index.js
@@ -0,0 +1,5 @@
+import { colors } from "./colors";
+import { layout } from "./spaces";
+import { typography } from "./fonts";
+import { sizes } from "./sizes";
+export const theme = {colors , layout, typography, sizes}
